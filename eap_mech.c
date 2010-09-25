4ac4b011020be93f1190bc0da2d8808e5dea48bc
Luke Howard <lukeh@padl.com>
2010-09-25 16:52:47 +0200

    cleanup, don't register server EAP methods

--- eap_mech.c
+++ eap_mech.c
@@ -141,113 +141,6 @@ eapPeerRegisterMethods(OM_uint32 *minor)
 }
 
 static OM_uint32
-eapServerRegisterMethods(OM_uint32 *minor)
-{
-    OM_uint32 ret = 0;
-
-#ifdef EAP_SERVER_IDENTITY
-    if (ret == 0)
-        ret = eap_server_identity_register();
-#endif /* EAP_SERVER_IDENTITY */
-
-#ifdef EAP_SERVER_MD5
-    if (ret == 0)
-        ret = eap_server_md5_register();
-#endif /* EAP_SERVER_MD5 */
-
-#ifdef EAP_SERVER_TLS
-    if (ret == 0)
-        ret = eap_server_tls_register();
-#endif /* EAP_SERVER_TLS */
-
-#ifdef EAP_SERVER_MSCHAPV2
-    if (ret == 0)
-        ret = eap_server_mschapv2_register();
-#endif /* EAP_SERVER_MSCHAPV2 */
-
-#ifdef EAP_SERVER_PEAP
-    if (ret == 0)
-        ret = eap_server_peap_register();
-#endif /* EAP_SERVER_PEAP */
-
-#ifdef EAP_SERVER_TLV
-    if (ret == 0)
-        ret = eap_server_tlv_register();
-#endif /* EAP_SERVER_TLV */
-#ifdef EAP_SERVER_GTC
-    if (ret == 0)
-        ret = eap_server_gtc_register();
-#endif /* EAP_SERVER_GTC */
-
-#ifdef EAP_SERVER_TTLS
-    if (ret == 0)
-        ret = eap_server_ttls_register();
-#endif /* EAP_SERVER_TTLS */
-
-#ifdef EAP_SERVER_SIM
-    if (ret == 0)
-        ret = eap_server_sim_register();
-#endif /* EAP_SERVER_SIM */
-
-#ifdef EAP_SERVER_AKA
-    if (ret == 0)
-        ret = eap_server_aka_register();
-#endif /* EAP_SERVER_AKA */
-
-#ifdef EAP_SERVER_AKA_PRIME
-    if (ret == 0)
-        ret = eap_server_aka_prime_register();
-#endif /* EAP_SERVER_AKA_PRIME */
-
-#ifdef EAP_SERVER_PAX
-    if (ret == 0)
-        ret = eap_server_pax_register();
-#endif /* EAP_SERVER_PAX */
-
-#ifdef EAP_SERVER_PSK
-    if (ret == 0)
-        ret = eap_server_psk_register();
-#endif /* EAP_SERVER_PSK */
-
-#ifdef EAP_SERVER_SAKE
-    if (ret == 0)
-        ret = eap_server_sake_register();
-#endif /* EAP_SERVER_SAKE */
-
-#ifdef EAP_SERVER_GPSK
-    if (ret == 0)
-        ret = eap_server_gpsk_register();
-#endif /* EAP_SERVER_GPSK */
-
-#ifdef EAP_SERVER_VENDOR_TEST
-    if (ret == 0)
-        ret = eap_server_vendor_test_register();
-#endif /* EAP_SERVER_VENDOR_TEST */
-
-#ifdef EAP_SERVER_FAST
-    if (ret == 0)
-        ret = eap_server_fast_register();
-#endif /* EAP_SERVER_FAST */
-
-#ifdef EAP_SERVER_WSC
-    if (ret == 0)
-        ret = eap_server_wsc_register();
-#endif /* EAP_SERVER_WSC */
-
-#ifdef EAP_SERVER_IKEV2
-    if (ret == 0)
-        ret = eap_server_ikev2_register();
-#endif /* EAP_SERVER_IKEV2 */
-
-#ifdef EAP_SERVER_TNC
-    if (ret == 0)
-        ret = eap_server_tnc_register();
-#endif /* EAP_SERVER_TNC */
-
-    return ret ? GSS_S_FAILURE : GSS_S_COMPLETE;
-}
-
-static OM_uint32
 gssEapInitLibEap(OM_uint32 *minor)
 {
     return eapPeerRegisterMethods(minor);
@@ -273,9 +166,6 @@ gssEapInit(void)
     major = gssEapInitLibRadius(&minor);
     assert(major == GSS_S_COMPLETE);
 
-    major = eapServerRegisterMethods(&minor);
-    assert(major == GSS_S_COMPLETE);
-
     major = gssEapRadiusAttrProviderInit(&minor);
     assert(major == GSS_S_COMPLETE);
 
@@ -299,8 +189,6 @@ gssEapFinalize(void)
     gssEapLocalAttrProviderFinalize(&minor);
     gssEapSamlAttrProvidersFinalize(&minor);
     gssEapRadiusAttrProviderFinalize(&minor);
-
     eap_peer_unregister_methods();
-    eap_server_unregister_methods();
 }
 
