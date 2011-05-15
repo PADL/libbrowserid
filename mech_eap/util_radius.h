53bb269be52adf0825340925e3c10daf221bb828
Luke Howard <lukeh@padl.com>
2011-05-16 01:03:46 +0200

    add MS-Windows-Auth-Data attribute

--- mech_eap/util_radius.h
+++ mech_eap/util_radius.h
@@ -164,6 +164,7 @@ gssEapRadiusMapError(OM_uint32 *minor,
 #define PW_GSS_ACCEPTOR_SERVICE_SPECIFIC    130
 #define PW_GSS_ACCEPTOR_REALM_NAME          131
 #define PW_SAML_AAA_ASSERTION               132
+#define PW_MS_WINDOWS_AUTH_DATA             133
 
 #define IS_RADIUS_ERROR(code)               ((code) >= ERROR_TABLE_BASE_rse && \
                                              (code) <= ERROR_TABLE_BASE_rse + RSE_TIMEOUT_IO)
