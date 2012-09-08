9ad1b8683942539d8e4ad0a46ea6a75c788c8a17
Luke Howard <lukeh@padl.com>
2012-09-08 12:28:30 +1000

    corresponding header change for gssEapPseudoRandom

--- mech_eap/gssapiP_eap.h
+++ mech_eap/gssapiP_eap.h
@@ -377,7 +377,6 @@ gssEapPseudoRandom(OM_uint32 *minor,
                    gss_ctx_id_t ctx,
                    int prf_key,
                    const gss_buffer_t prf_in,
-                   ssize_t desired_output_len,
                    gss_buffer_t prf_out);
 
 /* query_mechanism_info.c */
