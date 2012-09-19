4e8943a9731a43ba1cf2e8f990814e3f63d00f3b
Luke Howard <lukeh@padl.com>
2012-09-19 22:06:02 +1000

    fix indentation

--- mech_eap/init_sec_context.c
+++ mech_eap/init_sec_context.c
@@ -577,7 +577,7 @@ eapGssSmInitAcceptorName(OM_uint32 *minor,
         if (ctx->acceptorName != GSS_C_NO_NAME) {
             /* verify name hint matched asserted acceptor name  */
             major = gssEapCompareName(minor, nameHint,
-                                     ctx->acceptorName, &equal);
+                                      ctx->acceptorName, &equal);
             if (GSS_ERROR(major)) {
                 gss_release_name(&tmpMinor, &nameHint);
                 return major;
