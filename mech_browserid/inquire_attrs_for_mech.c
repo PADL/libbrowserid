bcb1743d95ec39002baee6b1c8ac327761788dba
Luke Howard <lukeh@padl.com>
2013-01-16 00:21:14 +1100

    cleanup mutual auth check

--- mech_browserid/inquire_attrs_for_mech.c
+++ mech_browserid/inquire_attrs_for_mech.c
@@ -93,18 +93,17 @@ gss_inquire_attrs_for_mech(OM_uint32 *minor,
     }
 
     /*
-     * This is a bit of a hack to see if we are configured for mutual
-     * authentication. Don't advertise it if we can't do it.
+     * XXX hack to advertise whether we can do mutual authentication.
+     * needs a better solution
      */
     err = BIDAcquireContext(BID_CONTEXT_GSS, &bidContext);
-    if (err == BID_S_OK)
+    if (err == BID_S_OK) {
         err = _BIDAcquireCache(bidContext, GSSBID_CONFIG_FILE,
                                BID_CACHE_FLAG_UNVERSIONED, &bidConfig);
-    if (err == BID_S_OK)
-        err = _BIDGetCacheObject(bidContext, bidConfig, "private-key", NULL);
-    if (err == BID_S_OK) {
-        fprintf(stdout, "mutual ok\n");
-        MA_SUPPORTED(GSS_C_MA_AUTH_TARG_INIT);
+        if (err == BID_S_OK &&
+            (_BIDGetCacheObject(bidContext, bidConfig, "ca-certificate", NULL) == BID_S_OK ||
+             _BIDGetCacheObject(bidContext, bidConfig, "ca-directory", NULL)   == BID_S_OK))
+            MA_SUPPORTED(GSS_C_MA_AUTH_TARG_INIT);
     }
 
     if (known_mech_attrs != NULL) {
