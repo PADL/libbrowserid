b1f722df45685c4396e19d671f27eab897858988
Luke Howard <lukeh@padl.com>
2013-11-23 02:21:47 +0000

    add clang-lib-build.sh

--- build/clang-heimdal-build.sh
+++ build/clang-heimdal-build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-OBJC=clang CC=clang CXX=clang++ OBJCFLAGS="-g -Wall -DGSSBID_DEBUG -Wno-deprecated-declarations" CXXFLAGS="-g -Wall -DGSSBID_DEBUG -Wno-deprecated-declarations" CFLAGS="-g -Wall -DGSSBID_DEBUG -Wno-deprecated-declarations" ./configure --with-krb5=/usr/local/heimdal --with-opensaml=no --with-shibresolver=no --with-shibsp=no
+OBJC=clang CC=clang CXX=clang++ OBJCFLAGS="-g -Wall -DGSSBID_DEBUG -Wno-deprecated-declarations" CXXFLAGS="-g -Wall -DGSSBID_DEBUG -Wno-deprecated-declarations" CFLAGS="-g -Wall -DGSSBID_DEBUG -Wno-deprecated-declarations" ./configure --with-krb5=/usr/local/heimdal --with-opensaml=no --with-shibresolver=no --with-shibsp=no --enable-gss-mech
