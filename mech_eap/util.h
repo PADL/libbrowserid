65bc46fcb4d9e8b0c0404a8d221e87528162ef14
Luke Howard <lukeh@padl.com>
2016-02-01 16:17:30 +1100

    add GSSEAP_UNUSED to krbFreeUnparsedName

--- mech_eap/util.h
+++ mech_eap/util.h
@@ -1062,7 +1062,7 @@ krbPrincUnparseServiceSpecifics(krb5_context krbContext, krb5_principal krbPrinc
 }
 
 static inline void
-krbFreeUnparsedName(krb5_context krbContext, gss_buffer_t nameBuf)
+krbFreeUnparsedName(krb5_context krbContext GSSEAP_UNUSED, gss_buffer_t nameBuf)
 {
 #ifdef HAVE_HEIMDAL_VERSION
     krb5_xfree((char *) nameBuf->value);
