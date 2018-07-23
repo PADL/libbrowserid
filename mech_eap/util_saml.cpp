e66e50f5b866c0be098466f12db42817a6776921
Alejandro Perez <alex.perez-mendez@jisc.ac.uk>
2018-07-23 10:21:09 +0200

    Remove unused include line that precluded using Shib SP 3.x

--- mech_eap/util_saml.cpp
+++ mech_eap/util_saml.cpp
@@ -43,7 +43,6 @@
 #include <xmltooling/XMLToolingConfig.h>
 #include <xmltooling/util/XMLHelper.h>
 #include <xmltooling/util/ParserPool.h>
-#include <xmltooling/util/DateTime.h>
 
 #include <saml/exceptions.h>
 #include <saml/SAMLConfig.h>
